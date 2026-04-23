d847d77eb133d1f783700a2a0c62030a94db32dc
Pedro Santo <pedro.bb.90@gmail.com>
2026-04-23 08:57:34 +0100

    comment out components that are not in use now and new "love" before they go to prod

--- src/components/Desktop/Desktop.tsx
+++ src/components/Desktop/Desktop.tsx
@@ -3,11 +3,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { AnimatePresence } from 'motion/react';
-import { Monitor, Users, Waves, Trash2 } from 'lucide-react';
-import type { WindowState } from '../../types';
-import DesktopIcon from '../DesktopIcon';
-import Window from '../Window';
+import { AnimatePresence } from "motion/react";
+import { Monitor, Users, Waves, Trash2 } from "lucide-react";
+import type { WindowState } from "../../types";
+import DesktopIcon from "../DesktopIcon";
+import Window from "../Window";
 
 interface DesktopProps {
   windows: WindowState[];
@@ -29,14 +29,18 @@ export default function Desktop({
   return (
     <main className="relative grow p-8 z-10">
       <div className="grid grid-cols-1 gap-6 w-max">
-        <DesktopIcon label="My Board" icon={Monitor} onClick={() => onOpenWindow('welcome')} />
-        <DesktopIcon label="The Lineup" icon={Users} onClick={() => onOpenWindow('projects')} />
-        <DesktopIcon label="Waves" icon={Waves} onClick={() => onOpenWindow('logs')} />
-        <DesktopIcon label="Trash" icon={Trash2} onClick={() => {}} />
+        <DesktopIcon
+          label="My Board"
+          icon={Monitor}
+          onClick={() => onOpenWindow("welcome")}
+        />
+        {/* <DesktopIcon label="The Lineup" icon={Users} onClick={() => onOpenWindow('projects')} /> */}
+        {/* <DesktopIcon label="Waves" icon={Waves} onClick={() => onOpenWindow('logs')} /> */}
+        {/* <DesktopIcon label="Trash" icon={Trash2} onClick={() => {}} /> */}
       </div>
 
       <AnimatePresence>
-        {windows.map(win => (
+        {windows.map((win) => (
           <Window
             key={win.id}
             win={win}
